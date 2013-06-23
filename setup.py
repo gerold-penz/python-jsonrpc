28df05e8923141872f4ba12b287ce4c5d3431137
Gerold Penz <gerold@halvar.at>
2013-06-23 19:49:31 +0200

    Setup

--- setup.py
+++ setup.py
@@ -7,57 +7,48 @@ Created
     2013-06-23 by Gerold - http://halvar.at/
 """
 
-# import os
-# from setuptools import setup, find_packages
-#
-# THISDIR = os.path.dirname(os.path.abspath(__file__))
-# os.chdir(THISDIR)
-#
-# VERSION = open("version.txt").readline().strip()
-# HOMEPAGE = "https://github.com/gerold-penz/cherrypy-jsonrpcserver"
-# DOWNLOAD_BASEURL = "https://github.com/gerold-penz/cherrypy-jsonrpcserver/raw/master/dist/"
-# DOWNLOAD_URL = DOWNLOAD_BASEURL + "cherrypy-jsonrpcserver-%s.tar.gz" % VERSION
-#
-#
-# setup(
-#     name = "cherrypy-jsonrpcserver",
-#     version = VERSION,
-#     description = (
-#         "CherryPy JSON-RPC-Server Tool - Simple To Use Python JSONRPC-Server"
-#     ),
-#     long_description = open("README.rst").read(),
-#     keywords = "CherryPy Web JSON JSON-RPC Tool",
-#     author = "Gerold Penz",
-#     author_email = "gerold@halvar.at",
-#     url = HOMEPAGE,
-#     download_url = DOWNLOAD_URL,
-#     packages = find_packages(),
-#     # data_files = [
-#     #     ["./yyy", ["_git_add.py"]],
-#     # ],
-#     classifiers = [
-#         #"Development Status :: 1 - Planning",
-#         # "Development Status :: 2 - Pre-Alpha",
-#         "Development Status :: 3 - Alpha",
-#         # "Development Status :: 4 - Beta",
-#         # "Development Status :: 5 - Production/Stable",
-#         "Environment :: Web Environment",
-#         "Framework :: CherryPy",
-#         "Intended Audience :: Developers",
-#         "Intended Audience :: System Administrators",
-#         "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
-#         "License :: OSI Approved :: MIT License",
-#         "Operating System :: OS Independent",
-#         "Programming Language :: Python :: 2",
-#         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-#         "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
-#         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-#         "Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
-#         "Topic :: Internet :: WWW/HTTP :: WSGI :: Server",
-#     ],
-#     install_requires = [
-#         # "distribute",
-#         # "cherrypy",
-#     ],
-# )
-#
+import os
+from setuptools import setup, find_packages
+
+THISDIR = os.path.dirname(os.path.abspath(__file__))
+os.chdir(THISDIR)
+
+VERSION = open("version.txt").readline().strip()
+HOMEPAGE = "https://github.com/gerold-penz/python-jsonrpc"
+DOWNLOAD_BASEURL = "https://github.com/gerold-penz/python-jsonrpc/raw/master/dist/"
+DOWNLOAD_URL = DOWNLOAD_BASEURL + "python-jsonrpc-%s.tar.gz" % VERSION
+
+
+setup(
+    name = "python-jsonrpc",
+    version = VERSION,
+    description = (
+        "Python JSON-RPC Library Client Server - Simple To Use Python JSONRPC-Library"
+    ),
+    long_description = open("README.rst").read(),
+    keywords = "JSON JSON-RPC Library Tool",
+    author = "Gerold Penz",
+    author_email = "gerold@halvar.at",
+    url = HOMEPAGE,
+    download_url = DOWNLOAD_URL,
+    packages = find_packages(),
+    # data_files = [
+    #     ["./yyy", ["_git_add.py"]],
+    # ],
+    classifiers = [
+        #"Development Status :: 1 - Planning",
+        # "Development Status :: 2 - Pre-Alpha",
+        # "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
+        # "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+    ],
+    install_requires = [
+        # "distribute",
+    ],
+)
+
